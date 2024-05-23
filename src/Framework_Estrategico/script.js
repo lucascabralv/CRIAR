169389e9843c5b82b1dd6ceffb4c0a26fcb1f94c
Lucas Cabral <lucascabralvieira@gmail.com>
2024-05-23 18:20:18 -0300

    Update script.js

--- src/Framework_Estrategico/script.js
+++ src/Framework_Estrategico/script.js
@@ -1,17 +1,30 @@
-// Returns solução OBJECT
 function findSolucao(slugToSearch) {
   return SOLUCOES.find((solucao) => solucao.slug === slugToSearch);
 }
 
-// HANDLE CLICK
+// Mostra as informações da solução no PopUp
+function loadSolucao(solucao){
+  const {nome, nome_curto, descricao, problema, solucao_padrao, efetividade_padrao, deslocamento_lateral, nova_hipotese, pilar, fator, flywheel, investimento, automacao, publicos_potenciais, potencial, efetividade}  = solucao;
+  
+  $("#text_nome").text(nome);
+  $("#text_problema").text(problema);
+  $("#text_solucao_padrao").text(solucao_padrao);
+  $("#text_nova_hipotese").text(nova_hipotese);
+  $("#text_solucao_CRIAR").text(nome);
+}
 
+
+/**------------------------------------------------------------------------
+ *h/                    CARD CLICK HANDLERS
+ *------------------------------------------------------------------------**/
 // 'INFO' BUTTON
 $(".button-info-fwe-card").click(function () {
   const slug = $(this).attr("solucao_slug");
   const solucao = findSolucao(slug);
+  // FILL POPUP WITH SOLUCAO INFO
+  loadSolucao(solucao);
   // OPEN POPUP
   openPopup();
-  // FILL POPUP WITH SOLUCAO INFO
 });
 
 // 'SELECT' BUTTON
@@ -19,13 +32,11 @@ $(".button-select-fwe-card").click(function () {
   const $card = $(this);
   // IF IT IS NOT SELECTED YET
   if (!$card.hasClass("selected")) {
-    // THEN - SELECT
     const slug = $card.attr("solucao_slug");
     const solucao = findSolucao(slug);
     // GET VALUES AND SET IN CRIAR
     // SET LOCAL STORAGE
   } else {
-    // THEN - UNSELECT
     // REMOVE VALUES IN CRIAR
     // SET LOCAL STORAGE
   }
@@ -33,10 +44,11 @@ $(".button-select-fwe-card").click(function () {
   $card.toggleClass("selected");
 });
 
-// POPUP OVERLAY
-$(".popup-fwe-overlay").click(closePopup);
-
-// POPUP CLOSE BUTTON
+/**------------------------------------------------------------------------
+ *h/                           POP UP
+ *------------------------------------------------------------------------**/
+// POPUP OVERLAY || CLOSE BUTTON
+$(".popup-fwe-close, .popup-fwe-overlay").click(closePopup);
 
 //POP UP OPEN
 function openPopup() {
